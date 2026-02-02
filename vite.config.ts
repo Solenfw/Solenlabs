4c5f84844202545d6bb555182751e8a874cae378
Solenfw <nhuyhoang1012005@gmail.com>
2026-02-03 01:30:58 +0700

    refresh pckgs

--- vite.config.ts
+++ vite.config.ts
@@ -27,13 +27,13 @@ export default defineConfig({
   },
   resolve: {
     alias: {
-      '@components': path.resolve(__dirname, 'src/components/'),
-      '@hooks': path.resolve(__dirname, 'src/hooks/'),
-      '@constants': path.resolve(__dirname, 'src/constants/'),
-      '@assets': path.resolve(__dirname, 'src/assets/'),
-      '@utils': path.resolve(__dirname, 'src/utils/'),
-      '@shaders': path.resolve(__dirname, 'src/shaders/'),
-      '@services': path.resolve(__dirname, 'src/services/'),
+      '@components': path.resolve(__dirname, 'src/components'),
+      '@hooks': path.resolve(__dirname, 'src/hooks'),
+      '@constants': path.resolve(__dirname, 'src/constants'),
+      '@assets': path.resolve(__dirname, 'src/assets'),
+      '@utils': path.resolve(__dirname, 'src/utils'),
+      '@shaders': path.resolve(__dirname, 'src/shaders'),
+      '@services': path.resolve(__dirname, 'src/services'),
     },
   },
 })
